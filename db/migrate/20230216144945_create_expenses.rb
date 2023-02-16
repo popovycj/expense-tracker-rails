2f284dcb02cb848b1b549142cc539a1e33d9a023
popovycj <marian.popovych.ua@gmail.com>
2023-02-16 16:50:52 +0200

    Fixed migrations

--- db/migrate/20230216144945_create_expenses.rb
+++ db/migrate/20230216144945_create_expenses.rb
@@ -1,7 +1,7 @@
 class CreateExpenses < ActiveRecord::Migration[7.0]
   def change
     create_table :expenses do |t|
-      t.decimal :amount, null: false, scale: 2
+      t.decimal :amount, null: false, precision: 10, scale: 2
       t.text :description
       t.references :user, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_134735) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_144945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "expenses", force: :cascade do |t|
+    t.decimal "amount", precision: 10, scale: 2, null: false
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.integer "visibility", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_expenses_on_category_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_134735) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "expenses", "categories"
+  add_foreign_key "expenses", "users"
 end
