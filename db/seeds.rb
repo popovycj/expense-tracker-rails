3529998b52b820479e172ebb10663658f805ffc5
popovycj <marian.popovych.ua@gmail.com>
2023-02-17 00:37:46 +0200

    Generated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+categories = ["Food", "Housing", "Transportation", "Entertainment", "Healthcare"]
+
+categories.each do |name|
+  Category.create!(name: name)
+  puts "Created category: #{name}"
+end
+
+# Create some users
+10.times do |i|
+  User.create!(
+    email: "user#{i+1}@example.com",
+    password: "password"
+  )
+
+  puts "Created user: user#{i+1}@example.com"
+end
+
+# Create expenses for each user with random categories, descriptions, amounts, and visibilities
+User.all.each do |user|
+  10.times do
+    expense = user.expenses.create!(
+      category: Category.all.sample,
+      description: Faker::Lorem.sentence(word_count: 3),
+      amount: rand(10.0..100.0).round(2),
+      visibility: rand(0..1),
+      created_at: Time.zone.local(2023, rand(1..2), rand(1..30), rand(0..23), rand(0..59), rand(0..59))
+    )
+
+    puts "Created expense: #{expense.description} (user: #{user.email}, category: #{expense.category.name})"
+  end
+end
