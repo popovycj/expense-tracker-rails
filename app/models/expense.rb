bf06cd57ed98215a263a368bcd04655717c73c30
popovycj <marian.popovych.ua@gmail.com>
2023-02-18 23:40:22 +0200

    Added enum for visibility of expense; Set routes

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,7 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  enum visibility: [ :private, :public ]
+  enum :visibility, [ :private, :public ], prefix: :visibility
 
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true, length: { maximum: 255 }
