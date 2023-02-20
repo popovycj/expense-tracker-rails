dab3486e8fec8872b40de434f2d8257ea2b21f60
popovycj <marian.popovych.ua@gmail.com>
2023-02-20 23:07:09 +0200

    Made ready for deploy on Render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def show_expenses
     @user = User.find_by(id: params[:id])
 
-    @expenses = @user.expenses.public_expenses
+    @expenses = @user.expenses.public_expenses.order(created_at: :desc)
 
     @expenses = @expenses.where(category: params[:category_id]) if params[:category_id].present?
     @expenses = @expenses.where(created_at: params[:start_date]..params[:end_date]) if params[:start_date].present? && params[:end_date].present?
